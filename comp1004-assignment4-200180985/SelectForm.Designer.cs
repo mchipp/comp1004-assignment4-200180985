822ca870f9d3f9115719118d92964c49dd6c26b1
Mark <200180985@student.georgianc.on.ca>
2016-11-17 16:44:43 -0500

    StartForm and SelectForm basic elements added.

---
+++ comp1004-assignment4-200180985/SelectForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace comp1004_assignment4_200180985
+{
+    partial class SelectForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ComputersDataGridView = new System.Windows.Forms.DataGridView();
+            this.SelectionLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ComputersDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ComputersDataGridView
+            // 
+            this.ComputersDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ComputersDataGridView.Location = new System.Drawing.Point(12, 12);
+            this.ComputersDataGridView.Name = "ComputersDataGridView";
+            this.ComputersDataGridView.Size = new System.Drawing.Size(568, 275);
+            this.ComputersDataGridView.TabIndex = 0;
+            // 
+            // SelectionLabel
+            // 
+            this.SelectionLabel.AutoSize = true;
+            this.SelectionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SelectionLabel.Location = new System.Drawing.Point(12, 297);
+            this.SelectionLabel.Name = "SelectionLabel";
+            this.SelectionLabel.Size = new System.Drawing.Size(106, 18);
+            this.SelectionLabel.TabIndex = 1;
+            this.SelectionLabel.Text = "Your selection:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(124, 298);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(258, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // NextButton
+            // 
+            this.NextButton.Location = new System.Drawing.Point(505, 297);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(75, 23);
+            this.NextButton.TabIndex = 3;
+            this.NextButton.Text = "Next";
+            this.NextButton.UseVisualStyleBackColor = true;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(424, 297);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelButton.TabIndex = 4;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // SelectForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 323);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.SelectionLabel);
+            this.Controls.Add(this.ComputersDataGridView);
+            this.Name = "SelectForm";
+            this.Text = "SelectForm";
+            ((System.ComponentModel.ISupportInitialize)(this.ComputersDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView ComputersDataGridView;
+        private System.Windows.Forms.Label SelectionLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button NextButton;
+        private System.Windows.Forms.Button CancelButton;
+    }
+}
---
+++ comp1004-assignment4-200180985/StartForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace comp1004_assignment4_200180985
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WelcomeLabel = new System.Windows.Forms.Label();
+            this.OrderLogoPictureBox = new System.Windows.Forms.PictureBox();
+            this.NewOrderButton = new System.Windows.Forms.Button();
+            this.OpenOrderButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.OrderLogoPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // WelcomeLabel
+            // 
+            this.WelcomeLabel.AutoSize = true;
+            this.WelcomeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WelcomeLabel.Location = new System.Drawing.Point(45, 27);
+            this.WelcomeLabel.Name = "WelcomeLabel";
+            this.WelcomeLabel.Size = new System.Drawing.Size(201, 20);
+            this.WelcomeLabel.TabIndex = 0;
+            this.WelcomeLabel.Text = "Order your computer today!";
+            // 
+            // OrderLogoPictureBox
+            // 
+            this.OrderLogoPictureBox.Location = new System.Drawing.Point(84, 56);
+            this.OrderLogoPictureBox.Name = "OrderLogoPictureBox";
+            this.OrderLogoPictureBox.Size = new System.Drawing.Size(125, 127);
+            this.OrderLogoPictureBox.TabIndex = 1;
+            this.OrderLogoPictureBox.TabStop = false;
+            // 
+            // NewOrderButton
+            // 
+            this.NewOrderButton.Location = new System.Drawing.Point(84, 201);
+            this.NewOrderButton.Name = "NewOrderButton";
+            this.NewOrderButton.Size = new System.Drawing.Size(125, 23);
+            this.NewOrderButton.TabIndex = 2;
+            this.NewOrderButton.Text = "Start a New Order";
+            this.NewOrderButton.UseVisualStyleBackColor = true;
+            // 
+            // OpenOrderButton
+            // 
+            this.OpenOrderButton.Location = new System.Drawing.Point(84, 230);
+            this.OpenOrderButton.Name = "OpenOrderButton";
+            this.OpenOrderButton.Size = new System.Drawing.Size(125, 23);
+            this.OpenOrderButton.TabIndex = 3;
+            this.OpenOrderButton.Text = "Open a Saved Order";
+            this.OpenOrderButton.UseVisualStyleBackColor = true;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(84, 259);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(125, 23);
+            this.ExitButton.TabIndex = 4;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 298);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.OpenOrderButton);
+            this.Controls.Add(this.NewOrderButton);
+            this.Controls.Add(this.OrderLogoPictureBox);
+            this.Controls.Add(this.WelcomeLabel);
+            this.Name = "StartForm";
+            this.Text = "StartForm";
+            ((System.ComponentModel.ISupportInitialize)(this.OrderLogoPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label WelcomeLabel;
+        private System.Windows.Forms.PictureBox OrderLogoPictureBox;
+        private System.Windows.Forms.Button NewOrderButton;
+        private System.Windows.Forms.Button OpenOrderButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
